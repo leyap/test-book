f1d33928fd577a71334d1ae1e8f528dc565df053
lisper <leyapin@gmail.com>
2015-12-05 20:25:45 +0800

    add dev2

--- hello.c
+++ hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void printHello();
+void printWorld();
+
 int main () {
 	printf("hello world");
 	return 0;
@@ -8,4 +11,9 @@ int main () {
 //
 void printHello() {
 	printf ("hello");
+}
+
+//
+void printWorld() {
+	printf ("hello\n");
 }
